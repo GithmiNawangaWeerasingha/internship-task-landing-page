125a7eeb86d2316332dcc1ae42e35dc41b696a86
Githmi Nawanga <90828906+GithmiNawangaWeerasingha@users.noreply.github.com>
2024-04-07 00:20:00 +0530

    Remove unused codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import FAQ from "./components/faq";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import Navbar from "./components/navbar";
-import FirstDiv from "./components/firstDiv";
-import SecondDiv from "./components/secondDiv";
 import Content from "./components/content";
 
 function App() {
